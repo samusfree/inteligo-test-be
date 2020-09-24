b9d6cef8b81a72ec714c5031c544772376bff1bd
Samuel Gonzales <sagonzales89@gmail.com>
2020-09-24 01:21:44 -0500

    initial project

---
+++ src/main/java/pe/inteligo/test/InteligoTestBeApplication.java
@@ -0,0 +1,13 @@
+package pe.inteligo.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteligoTestBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteligoTestBeApplication.class, args);
+	}
+
+}
